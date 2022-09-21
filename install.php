409095744fac7ca74793207dcfa1385aae58f482
Brevis Nguyen <99793625+brevis-ng@users.noreply.github.com>
2022-09-21 11:11:55 +0800

    Fix php version require

--- install.php
+++ install.php
@@ -8,7 +8,7 @@
 */
 header('Content-Type:text/html;charset=utf-8');
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.5.0','<'))  die('PHP版本需要>=5.5，请升级【PHP version requires > = 5.5，please upgrade】');
+if(version_compare(PHP_VERSION,'7.4','<'))  die('PHP版本需要>=7.4，请升级【PHP version requires > = 7.4，please upgrade】');
 //超时时间
 @ini_set('max_execution_time', '0');
 //内存限制 取消内存限制
