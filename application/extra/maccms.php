81088d9c7ba34a20f463b3e558e8673553819e9a
brevis-ng <brevisnguyen@gmail.com>
2022-12-22 16:53:14 +0800

    clear .gitignore

--- application/extra/maccms.php
+++ application/extra/maccms.php
@@ -192,7 +192,7 @@ return array (
   'interface' => 
   array (
     'status' => 0,
-    'pass' => 'WG3X7C4U8WOK0KWY',
+    'pass' => 'XEZPQX734A9JETFY',
     'vodtype' => '动作片=动作',
     'arttype' => '头条=头条',
     'actortype' => '',
