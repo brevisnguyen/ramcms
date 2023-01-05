fde51ec5e2b95034dff5cf8a886b905d668a4a22
brevis-ng <brevisnguyen@gmail.com>
2023-01-05 19:28:58 +0800

    update player

--- application/extra/vodplayer.php
+++ application/extra/vodplayer.php
@@ -1,18 +1,5 @@
 <?php
 return array (
-  'ngm3u8' => 
-  array (
-    'status' => '1',
-    'from' => 'ngm3u8',
-    'show' => 'Vietsub #1',
-    'des' => 'Nguon M3U8',
-    'target' => '_self',
-    'ps' => '0',
-    'parse' => '',
-    'sort' => '910',
-    'tip' => 'Trình phát HSL có hỗ trợ TVC',
-    'id' => 'ngm3u8',
-  ),
   'nguon' => 
   array (
     'status' => '1',
@@ -25,5 +12,18 @@ return array (
     'sort' => '909',
     'tip' => 'Trình phát embed có hỗ trợ TVC',
     'id' => 'nguon',
+  ),
+  'ngm3u8' => 
+  array (
+    'status' => '1',
+    'from' => 'ngm3u8',
+    'show' => 'Vietsub #1',
+    'des' => 'Nguon M3U8',
+    'target' => '_self',
+    'ps' => '0',
+    'parse' => '',
+    'sort' => '908',
+    'tip' => 'Trình phát HSL có hỗ trợ TVC',
+    'id' => 'ngm3u8',
   ),
 );
